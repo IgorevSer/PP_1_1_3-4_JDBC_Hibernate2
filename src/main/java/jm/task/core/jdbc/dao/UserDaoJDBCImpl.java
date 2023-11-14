5532c5aff7c15c06cb9f8ab0a3794ade594fe194
SergeyZ31 <zyzin19942019@yandex.ru>
2023-11-14 10:31:10 +0300

    commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -18,6 +18,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
         String sql = "CREATE TABLE IF NOT EXISTS User " +
                 "(id INT AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age TINYINT, PRIMARY KEY (id))";
+        System.out.println("User с именем – "+ User.class.getName() +" добавлен в базу данных");
         try (Connection connection = Util.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
